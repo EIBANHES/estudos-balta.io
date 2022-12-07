1c8b221cd6a70f84d7c5d96ed9dfd26515346604
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2022-12-06 22:24:18 -0400

    consumindo api

--- petshop/src/app/pages/store/products-page/products-page.component.ts
+++ petshop/src/app/pages/store/products-page/products-page.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.css']
 })
-export class ProductsPageComponent implements OnInit {
+export class ProductsPageComponent implements OnInit { // implements OnInit -> Obriga o component ser construido antes
 
-  constructor() { }
+  //utilização do async
+  public products$: Observable<any[]>; //variavel
+
+  constructor(private data: DataService) { } //itens de variaveis ou coisas que nao exigem mto processamento sao setadas no constructor, pq pode travar o component
 
   ngOnInit() {
-    
+    this.products$ =  this.data.getProducts();
   }
 
 }
