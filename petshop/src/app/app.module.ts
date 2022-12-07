b01ad8d70b156532fb3ea9a89132ec85f62d76fa
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2022-12-06 20:30:35 -0400

    paginas implementadas

--- petshop/src/app/app.module.ts
+++ petshop/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
+import { PetsPageComponent } from './pets-page/pets-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,9 @@ import { SignupPageComponent } from './signup-page/signup-page.component';
     LoginPageComponent,
     ResetPasswordPageComponent,
     SignupPageComponent,
+    PetsPageComponent,
+    ProductsPageComponent,
+    CartPageComponent,
   ],
   imports: [
     BrowserModule,
