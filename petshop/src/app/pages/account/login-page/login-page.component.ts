c46f5afb9b451049725b53db8bde03d4cf088bae
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2022-12-12 22:07:58 -0400

    estudo

--- petshop/src/app/pages/account/login-page/login-page.component.ts
+++ petshop/src/app/pages/account/login-page/login-page.component.ts
@@ -10,40 +10,65 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class LoginPageComponent implements OnInit {
   public form: FormGroup;
+  public busy = false;
   constructor(
-    private service: DataService,
+    private service: DataService,  //injeção de dependencia
     private fb: FormBuilder
-    ) { 
-      this.form = fb.group({
-        username: ['', Validators.compose([
-          Validators.minLength(11),
-          Validators.maxLength(11),
-          Validators.required
-        ])],
-        password: ['', Validators.compose([
-          Validators.minLength(6),
-          Validators.maxLength(20),
-          Validators.required
-        ])]
-      });
-    }
+  ) {
+    this.form = fb.group({
+      username: ['', Validators.compose([
+        Validators.minLength(11),
+        Validators.maxLength(11),
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        Validators.required
+      ])]
+    });
+  }
 
   ngOnInit() {
-  }
-     //Variavel global, session storage, local storage
-     submit(){ //armazenamento do token
+    const token = localStorage.getItem('petshop.token'); //tenta pegar o token
+
+    if (token) { //se conseguir pegar o token no localStorage
+      this.busy = true;
       this
         .service
-        .authenticate(this.form.value)
-        .subscribe(
-          (data: any) =>{
-          localStorage.setItem('petshop.token', data.token)
-      },
-      (err)=>{
-        console.log(err);
-      }
+        .refreshToken() //pega o metodo do service e passa um valor -> data
+        .subscribe( //streamming de dados, no post n tem problema de demora
+          (data: any) => {
+            localStorage.setItem('petshop.token', data.token) //token armazenado quando logar 
+            this.busy = false;
+          },
+          (err) => {
+            localStorage.clear(); //limpa as informações do localStorage
+            this.busy = false;
+          }
+        );
+    }
+  }
+  //Variavel global -> Cada recarregando teria que logar dnv na aplicação
+  //session storage -> Se o  usuário fechar a aba ele perde o token
+  //local storage  -> Pode reiniciar a maquina e  tudo que o token nao vai ser perdido
+
+  submit() { //armazenamento do token
+    this.busy = true;
+    this
+      .service
+      .authenticate(this.form.value) //pega o metodo do service e passa um valor -> data
+      .subscribe( //streamming de dados, no post n tem problema de demora
+        (data: any) => {
+          localStorage.setItem('petshop.token', data.token) //token armazenado quando logar 
+          this.busy = false;
+        },
+        (err) => {
+          console.log(err);
+          this.busy = true;
+        }
       );
-     }
+  }
 
 
 }
--- petshop/src/app/services/data.service.ts
+++ petshop/src/app/services/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Product } from '../models/product.models';
 
 @Injectable({ // consigo injetar essa classe em outros components, realizar uma injeção de dependencia
@@ -9,11 +9,22 @@ export class DataService { //classe
     public url = 'http://localhost:3000/v1'
     constructor(private http: HttpClient){ } //dependencia do http client
 
+    //compondo os cabeçalhos da aplicação
+    public composeHeaders(){
+        const token = localStorage.getItem('petshop.token'); //retorna o token de login da aplicação
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);   //Cabeçalho  -> Authorization, valor -> bearer, token
+        return headers;
+    }
+
     getProducts(){
-        return this.http.get<Product[]>(`${this.url}/products`); //retorna um array de qualquer tipo
+        return this.http.get<Product[]>(`${this.url}/products`); //retorna um array de tipo Product
     }
 
     authenticate(data){
         return this.http.post(`${this.url}/accounts/authenticate`, data);
+    }
+
+    refreshToken(){
+        return this.http.post(`${this.url}/accounts/refresh-token`, null, { headers: this.composeHeaders()});  //{ headers: this.composeHeaders()}  -> compoe a requisição
     }
 }
